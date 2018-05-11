10bc867d04a48918e53f2edb0fe6de7244d4ebc1
lhenrique <luiz.henrique@watchguard.com>
2018-05-11 09:16:35 -0300

    removed changeme, and removed unused constant.

--- src/main/java/br/pegz/tutorials/rightcourt/configuration/CourtConfiguration.java
+++ src/main/java/br/pegz/tutorials/rightcourt/configuration/CourtConfiguration.java
@@ -7,14 +7,13 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
-@Component
+@Configuration
 public class CourtConfiguration {
 
     @Autowired
     private RestTemplate restTemplate;
 
     public static String LEFT_COURT_BASE  = "http://localhost:5000/left";
-    public static String LEFT_SERVE = LEFT_COURT_BASE + "/serve";
     public static String LEFT_PLAY = LEFT_COURT_BASE + "/play";
 
 
